3bff00f1c7002b14618014200093547a5d706031
Martin Halamíček <martin@halamicek.cz>
2014-04-08 10:34:33 +0200

    elasticsearch memory to 80%

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,7 @@
 default['rsyslog']['server']                    = true
 default['rsyslog']['protocol']                  = 'udp'
 default['rsyslog']['port']                      = 514
+
+
+allocated_memory = "#{(node.memory.total.to_i * 0.8 ).floor / 1024}m"
+default['elasticsearch'][:allocated_memory] = allocated_memory
